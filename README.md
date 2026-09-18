09f11d217e88139f2ef71fce87ca96a8e11b3045
autumnswank <autumnswank1995@gmail.com>
2026-09-18 08:54:46 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # devops_challenge
 Тестовое задание
+
+
+
+kind_deploy - 2 часть
---
+++ kind_deploy/erase.sh
@@ -0,0 +1,3 @@
+kind delete cluster --name nginx-cluster
+docker rmi -f helloworld-js:v1
+docker rmi -f nginx-proxy:v1
